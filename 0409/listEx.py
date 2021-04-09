d11d2f649052e5c035d8922e87efa779fc339fe1
leesoonhee72 <80086456+leesoonhee72@users.noreply.github.com>
2021-04-09 15:55:22 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,15 @@
+fstName=['L','e','e']
+sndName=["Soon","Hee"]
+stdNum=[2,0,2,1,0,6,0,0,5]
+
+print(fstName)
+print(sndName)
+#화면에 L,S,H를 출력하는 프로그래을 작성하시오
+print(fstName[0]) #L
+print(sndName[0][0]) #S
+print(sndName[1][0]) #H
+
+
+
+
+
