4c2e7fc3940a8acc411225d5ee1959de002a21ec
leesoonhee72 <80086456+leesoonhee72@users.noreply.github.com>
2021-05-14 15:49:00 +0900

    Add files via upload

---
+++ examturtle01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 15:19:03 2021
+
+@author: Mac_1
+"""
+
+import turtle as t
+
+t.clear()
+t.shape('classic')
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.circle(100)
---
+++ manyArgs.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 14:27:30 2021
+
+@author: Mac_1
+"""
+
+def add(a,b):
+    return a+b
+c=add(1,2)
+print(c)
